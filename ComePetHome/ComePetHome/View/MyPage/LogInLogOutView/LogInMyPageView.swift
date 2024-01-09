89f516d14352fd62f5c53b4c1ac2514ac978c8e9
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2024-01-10 00:39:42 +0900

    [Add] 로그인 상태, 비로그인 상태 나눈 뷰 작성

--- ComePetHome/ComePetHome/View/MyPage/LogInLogOutView/LogInMyPageView.swift
+++ ComePetHome/ComePetHome/View/MyPage/LogInLogOutView/LogInMyPageView.swift
@@ -9,7 +9,77 @@ import SwiftUI
 
 struct LogInMyPageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: .screenWidth * 0.23)
+                        .frame(width: .screenWidth * 0.23, height: .screenWidth * 0.23)
+                        .foregroundStyle(Color("MainColor"))
+                    Image("ComePetHomeLogoImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: .screenWidth * 0.2)
+                }
+                VStack(alignment: .leading) {
+                    Text("홍길동")
+                        .font(.TheJamsilRegular18)
+                        .padding(EdgeInsets(top: 0, leading: .screenWidth * 0.1, bottom: .screenWidth * 0.005, trailing: 0))
+                    Text("Hong1234")
+                        .font(.TheJamsilRegular14)
+                        .padding(EdgeInsets(top: 0, leading: .screenWidth * 0.1, bottom: .screenWidth * 0.005, trailing: 0))
+                        .foregroundStyle(Color.black.opacity(0.5))
+                }
+                Spacer()
+                
+            }
+            .padding(EdgeInsets(top: 0, leading: .screenWidth * 0.5 - .screenWidth * 0.3, bottom: 0, trailing: 10))
+            Rectangle()
+                .frame(width: .screenWidth, height: 1)
+                .foregroundStyle(Color("MainColor"))
+                .padding(EdgeInsets(top: .screenWidth * 0.05, leading: 0, bottom: .screenWidth * 0.05, trailing: 0))
+            VStack {
+                NavigationLink {
+                    EditMyInformaitionView()
+                } label: {
+                    HStack {
+                        Text("내 정보 수정")
+                            .font(.TheJamsilMedium24)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .font(.TheJamsilMedium24)
+                    }
+                }
+                .foregroundStyle(Color("FontBlackWhiteColor"))
+                .padding(EdgeInsets(top: 0, leading: .screenWidth * 0.05, bottom: .screenWidth * 0.1, trailing: 10))
+                HStack {
+                    NavigationLink {
+                        LikeAnimalView()
+                    } label: {
+                        Text("관심동물")
+                            .font(.TheJamsilMedium24)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .font(.TheJamsilMedium24)
+                    }
+                }
+                .foregroundStyle(Color("FontBlackWhiteColor"))
+                .padding(EdgeInsets(top: 0, leading: .screenWidth * 0.05, bottom: .screenWidth * 0.1, trailing: 10))
+                HStack {
+                    NavigationLink {
+                        NoticeBoradWrite()
+                    } label: {
+                        Text("게시판 작성글/댓글")
+                            .font(.TheJamsilMedium24)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .font(.TheJamsilMedium24)
+                    }
+                }
+                .foregroundStyle(Color("FontBlackWhiteColor"))
+                .padding(EdgeInsets(top: 0, leading: .screenWidth * 0.05, bottom: .screenWidth * 0.05, trailing: 10))
+            }
+            Spacer()
+        }
     }
 }
 
--- ComePetHome/ComePetHome/View/MyPage/LogInLogOutView/LogOutMyPageView.swift
+++ ComePetHome/ComePetHome/View/MyPage/LogInLogOutView/LogOutMyPageView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct LogOutMyPageView: View {
+    @State private var isSheet = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: .screenWidth * 0.23)
+                    .frame(width: .screenWidth * 0.23, height: .screenWidth * 0.23)
+                    .foregroundStyle(Color("MainColor"))
+                Image("ComePetHomeLogoImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: .screenWidth * 0.2)
+            }
+            .padding([.bottom], .screenWidth * 0.05)
+            Text("로그인 후 이용해 주세요")
+                .font(.TheJamsilBold24)
+                .padding([.bottom], .screenWidth * 0.1)
+            Button {
+                isSheet = true
+                
+            } label: {
+                Text("로그인 하러 가기")
+                    .font(.TheJamsilRegular16)
+                    .foregroundStyle(Color.black.opacity(0.5))
+                Image(systemName: "chevron.right")
+                    .font(.TheJamsilRegular16)
+                    .foregroundStyle(Color.black.opacity(0.5))
+            }
+            .sheet(isPresented: $isSheet) {
+                LogInView()
+            }
+        }
     }
 }
 
