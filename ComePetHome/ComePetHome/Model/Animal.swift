152ecaab6c250e553a1d56ca0e37bae1905ed155
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2024-01-31 23:53:18 +0900

    .

--- ComePetHome/ComePetHome/Model/Animal.swift
+++ ComePetHome/ComePetHome/Model/Animal.swift
@@ -7,24 +7,7 @@
 
 import Foundation
 
-struct Animal {
-    let animalID: Int
-    let animalName: String
-    let enlistmentDate: Date
-    let species: String
-    let age: Int
-    let weight: Double
-    let adpStatus: String
-    let temporaryProtectionStatus: String
-    let introURL: String
-    let introContents: String
-    let temporaryProtectionContents: String
-    let thumbnailURL: String
-    let createdAt: Date
-    let updatedAt: Date
-}
-
-struct Pet: Codable {
+struct Pet: Codable, Hashable {
     let pet_id: Int
     let name: String
     let center: String
@@ -34,4 +17,22 @@ struct Pet: Codable {
     let age: String
     let adp_status: String
     let like: Bool
+}
+
+struct PetDetail: Codable {
+    let pet_id: Int
+    let name: String
+    let center: String
+    let enlistment_date: String
+    let breeds: String
+    let sex: String
+    let age: String
+    let adp_status: String
+    let species: String
+    let weight: Double
+    let intro_url: String
+    let intro_contents: String
+    let thumbnail_url: String
+    let created_at: String
+    let updated_at: String
 }
