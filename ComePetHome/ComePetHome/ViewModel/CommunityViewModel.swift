128a688546a9d97312b457ee68e2f38d5ff08eac
jin_hyoung <37445660+mhn03148@users.noreply.github.com>
2024-02-15 21:13:33 +0900

    .

--- ComePetHome/ComePetHome/ViewModel/CommunityViewModel.swift
+++ ComePetHome/ComePetHome/ViewModel/CommunityViewModel.swift
@@ -35,7 +35,7 @@ class CommunityViewModel: ObservableObject {
             do {
                 let decoder = JSONDecoder()
                 let decodedcommunities = try decoder.decode([Community].self, from: data)
-               
+                
                 DispatchQueue.main.async {
                     self.communityList = decodedcommunities
                     print(self.communityList)
@@ -70,7 +70,7 @@ class CommunityViewModel: ObservableObject {
             do {
                 let decoder = JSONDecoder()
                 let decodedcommunityInfo = try decoder.decode(CommunityInfo.self, from: data)
-               
+                
                 DispatchQueue.main.async {
                     self.communityInfo = decodedcommunityInfo
                     print(self.communityInfo as Any)
@@ -81,8 +81,50 @@ class CommunityViewModel: ObservableObject {
         }
         task.resume()
     }
+    
+    func postComment(communityId: Int, comment: String) {
+        let parameters = [
+            "contents": comment,
+            "article_id": communityId
+        ] as [String : Any]
+        do {
+            let postData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+            guard let url = URL(string: "http://54.180.142.14:9001/pet/community/comment") else { return }
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = postData
+            request.addValue("*/*", forHTTPHeaderField: "accept")
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data else {
+                    print(String(describing: error))
+                    return
+                }
+                print(String(data: data, encoding: .utf8)!)
+                let message = String(data: data, encoding: .utf8)!
+                if let jsonData = message.data(using: .utf8) {
+                    do {
+                        if let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                            //print(jsonObject)
+                            if let message = jsonObject["message"] as? String {
+                                print("\(message)")
+                            }
+                            // 여기서 딕셔너리를 사용하여 원하는 작업을 수행할 수 있습니다.
+                            if let message = jsonObject["userId"] as? String {
+                                print("Message: \(message)")
+                            }
+                        }
+                    } catch {
+                        print("Error decoding JSON: \(error)")
+                    }
+                }
+            }
+            task.resume()
+        } catch {
+            print("Error decoding JSON: \(error)")
+        }
+    }
 }
-
 struct ContentView2: View {
     @StateObject var communityVM = CommunityViewModel()
     var body: some View {
@@ -94,6 +136,7 @@ struct ContentView2: View {
         }
     }
 }
+
 #Preview {
     ContentView2()
 }
